fa56f31133ef83c2a361cfe2b3abb4ac6b19c015
tamana-alizada <t_alizada@kbtu.kz>
2026-03-05 01:42:30 +0500

    Add files via upload

---
+++ Lab6/album-browser-app/src/app/component/not-found-component/not-found-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-component',
+  imports: [],
+  template: `
+    <p>
+      not-found-component works!
+    </p>
+  `,
+  styles: ``,
+})
+export class NotFoundComponent {
+
+}
